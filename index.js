a2d2d762c9f70be570084bef3a20cd848cb7fd88
tinkoh <givensuman@gmail.com>
2021-06-02 16:59:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const quotes = require('./quotes.json');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.get('/', (req, res) => {
     let quote = quotes[Math.floor(Math.random() * quotes.length)];
-    let json = JSON.stringify({ quote: quote });
+    let json = JSON.stringify({ quote : quote });
     res.send(json);
 });
+
 let port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// npm start index.js
