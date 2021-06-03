7d4aedc5c82fca7e9668fc9dd383eb542f8d17f0
tinkoh <givensuman@gmail.com>
2021-06-03 12:23:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const quotes = require('./quotes.json');
 const express = require('express');
+const subdomain = require('express-subdomain');
 const cors = require('cors');
-const app = express();
 
+
+// Create app and router
+const app = express();
+let router = express.Router();
+// Registers subdomain middleware for 'api.xyz.xyz'
+app.use(subdomain('api', router));
 app.use(cors());
-app.get('/', (req, res) => {
+// Fallback for all other routes to static page in 'public' dir
+app.use(express.static('public'));
+// Main express app
+app.get('/', (req, res, next) => {
+    if (req.subdomains.length > 0) {
+        next();
+    } else {
+        res.send('Hello, world');
+    }
+});
+// Main router app
+router.get('/', (req, res) => {
     let quote = quotes[Math.floor(Math.random() * quotes.length)];
     let json = JSON.stringify({ quote : quote });
     res.send(json);
+});
+router.get('/*', (req, res) => {
+    res.json({ error : {
+        code : '404',
+        message : 'This API does not have active endpoints.'
+    }});
 });
 
 let port = process.env.PORT || 3000;
