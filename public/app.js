1e1e78c2692ca97d0ccf0028791b344870a186c0
Given Suman <givensuman@gmail.com>
2021-06-21 07:39:44 -0400

    Remove reasons element

--- public/app.js
+++ public/app.js
@@ -21,10 +21,10 @@ let codeBlocks = {
                     &nbsp;&nbsp;&nbsp;.then(res => res.json())<br/>
                     &nbsp;&nbsp;&nbsp;.then(data => console.log(data.quote);`,
     'python' : `import requests<br>
-        &nbsp;&nbsp;quote = (requests<br>
-        &nbsp;&nbsp;.get('https://api.johnmulapi.com')<br>
-        &nbsp;&nbsp;.json())<br>
-        &nbsp;&nbsp;print(quote['quote'])`
+                &nbsp;&nbsp;quote = (requests<br>
+                &nbsp;&nbsp;.get('https://api.johnmulapi.com')<br>
+                &nbsp;&nbsp;.json())<br>
+                &nbsp;&nbsp;print(quote['quote'])`
 }
 let codeArray = document.getElementsByClassName('code-button')
     for (i=0; i<codeArray.length; i++) {
@@ -41,37 +41,11 @@ let codeArray = document.getElementsByClassName('code-button')
         })
     }
 
-// Start page with a random reason for FAQ page
-const reasons = [
-    'To make the world a better place.',
-    'They asked Edison the same thing.',
-    "If you have to ask, you'll never know.",
-    "Great innovation doesn't need a reason.",
-    'Would you ask Steve Jobs that?'
-]
-let currentReason = ''; // Ensures a different reason is loaded each time
-function getReason() {
-    let rnd = Math.floor(Math.random() * reasons.length)
-    if (reasons[rnd] === currentReason) {
-        if (rnd >= reasons.length) {
-            rnd = 0;
-        } else {
-            rnd = rnd + 1;
-        }
-    } 
-    if (rnd >= reasons.length) {
-        rnd = 0;
-    }
-    let newReason = reasons[rnd];
-    getEle('reason').innerHTML = newReason;
-    currentReason = newReason;
-}
 // Opens/closes FAQ window on FAQ interaction
 faqStatus = false;
 getEle('faq').addEventListener('click', () => {
     let faqDiv = getEle('faq-div');
     if (!faqStatus) {
-        getReason();
         faqDiv.style.height = '470px';
         faqStatus = true;
     } else {
