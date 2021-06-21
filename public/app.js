e6cd20722b3f28d983aaf74f109e595ca5914ae8
Given Suman <givensuman@gmail.com>
2021-06-21 09:45:05 -0400

    Update FAQ transform

--- public/app.js
+++ public/app.js
@@ -46,7 +46,7 @@ faqStatus = false;
 getEle('faq').addEventListener('click', () => {
     let faqDiv = getEle('faq-div');
     if (!faqStatus) {
-        faqDiv.style.height = '470px';
+        faqDiv.style.height = '275px';
         faqStatus = true;
     } else {
         faqDiv.style.height = '0px';
