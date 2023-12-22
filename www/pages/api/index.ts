f4c51adf2b9e9b42fe8ed9dcf029b5594a77ddb0
givensuman <givensuman@duck.com>
2023-12-21 20:20:31 -0500

    Change API

--- www/pages/api/index.ts
+++ www/pages/api/index.ts
@@ -1,67 +1,11 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import allQuotes from '../../quotes/quotes.min.json'
-
-export type ResType = {
-    data: string | string[] | null
-}
-
-export type ReqType = {
-    quantity?: string,
-    maxLength?: string,
-    minLength?: string
-}
+import type { NextApiRequest, NextApiResponse } from "next";
+import fetch, { Response } from "../../../quotes/fetch";
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ResType>
+  res: NextApiResponse<Response>
 ) {
-    const { 
-        quantity, maxLength, minLength
-    } = req.query as ReqType
+  const api_response = fetch();
 
-    const getQuote = (): ResType["data"] => {
-        let quotes: string[] = allQuotes
-
-        // Filter quotes by optional max/min length parameters
-        if (maxLength && Number(maxLength)) {
-            quotes = quotes.filter(quote => quote.length <= Number(maxLength))
-        }
-        if (minLength && Number(minLength)) {
-            quotes = quotes.filter(quote => quote.length >= Number(minLength))
-        }
-
-        // If there are no quotes in that range, return null
-        if (!(quotes.length > 0)) {
-            return null
-        }
-
-        // If no quantity provided, use first random index
-        if (!quantity || !Number(quantity)) {
-            return quotes[Math.floor(Math.random() * quotes.length)]
-        }
-
-        // Else loop over quantity to get that many unique indices
-        const randIndices: number[] = []
-        let i = 0
-        while (i < Number(quantity)) {
-            let randomIndex = Math.floor(Math.random() * quotes.length)
-
-            if (!randIndices.includes(randomIndex)) {
-                randIndices.push(randomIndex)
-                i++
-            }
-        }
-
-        // Return array of quotes of those indices
-        return [...Array(Number(quantity))]
-            .map((_, i) => {
-                return quotes[randIndices[i]]
-            })
-            // Filter out nulls
-            .filter(quote => !!quote)
-    }
-
-    res.status(200).json({ 
-        data: getQuote() 
-    })
+  res.status(api_response.status).json(api_response);
 }
