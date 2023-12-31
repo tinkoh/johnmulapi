ce32918fc71425a3d20235222ca0d16413802f41
givensuman <givensuman@duck.com>
2023-12-31 15:26:49 -0500

    Add working meta tags

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import theme from "../styles/theme";
 
+import { Helmet } from "react-helmet";
+
 import Heading from "./lib/Heading";
 import Image from "./lib/Image";
 import Controls from "./lib/Controls";
@@ -15,21 +17,40 @@ import Contexts from "./lib/hooks/Contexts";
 
 function App() {
   return (
-    <ChakraProvider theme={extendTheme(theme)}>
-      <Contexts>
-        <Wrapper>
-          <Toolbar />
-          <Heading />
-          <Controls />
-          <Quote />
-          <QuoteList />
-          <HowToUse />
-          <Image />
-          <Footer />
-          <MouseFollow />
-        </Wrapper>
-      </Contexts>
-    </ChakraProvider>
+    <>
+      <Helmet>
+        <link rel="icon" type="image/png" href="/mulaney_head.png" />
+        <title>johnmulapi</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/mulaney_head.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/mulaney_head.png"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/mulaney_head.png"
+          sizes="16x16"
+        />
+      </Helmet>
+      <ChakraProvider theme={extendTheme(theme)}>
+        <Contexts>
+          <Wrapper>
+            <Toolbar />
+            <Heading />
+            <Controls />
+            <Quote />
+            <QuoteList />
+            <HowToUse />
+            <Image />
+            <Footer />
+            <MouseFollow />
+          </Wrapper>
+        </Contexts>
+      </ChakraProvider>
+    </>
   );
 }
 
